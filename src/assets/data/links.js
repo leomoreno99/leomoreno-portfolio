4f7faea0ae1477fa40989b2f349d0d0792a1b377
Leo Moreno <65966386+leomoreno99@users.noreply.github.com>
2023-06-26 23:55:17 -0300

    Update links.js

--- src/assets/data/links.js
+++ src/assets/data/links.js
@@ -1,35 +1,9 @@
 module.exports = {
-    cvURL: 'https://drive.google.com/file/d/1mwJeE8RXsavyTSYiJiVVOG7kGD4DMx6r/view?usp=sharing',
+    cvURL: 'https://drive.google.com/file/d/1-Rjtk-7pGh1uJieR8V_sX-IgVe_pHUdj/view',
     githubURL: 'https://github.com/leomoreno99',
     linkedinURL: 'https://www.linkedin.com/in/leomoreno99/',
     instagramURL: 'https://www.instagram.com/leo_moreno99/',
     wspNumber: '5493804261732',
     email: 'leomoreno330@gmail.com',
-
-    githubPortfolioUrl: 'https://github.com/leomoreno99/leomoreno-portfolio',
-
-  
-    socialMedia: [
-      {
-        name: 'GitHub',
-        url: 'https://github.com/bchiang7',
-      },
-      {
-        name: 'Instagram',
-        url: 'https://www.instagram.com/bchiang7',
-      },
-      {
-        name: 'Twitter',
-        url: 'https://twitter.com/bchiang7',
-      },
-      {
-        name: 'Linkedin',
-        url: 'https://www.linkedin.com/in/bchiang7',
-      },
-      {
-        name: 'Codepen',
-        url: 'https://codepen.io/bchiang7',
-      },
-    ],
-  
+    githubPortfolioUrl: 'https://github.com/leomoreno99/leomoreno-portfolio'
   };
